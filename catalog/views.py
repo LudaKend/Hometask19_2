91c53531f5fda6314b7b8f0589669948bbc6ac54
LudaKend <Ludakend@gmail.com>
2023-11-13 09:26:51 +0300

    домашнее задание к уроку 19.2 POST - запрос

--- catalog/views.py
+++ catalog/views.py
@@ -5,4 +5,11 @@ def index_home_page(requests):
     return render(requests, 'home_page.html')
 
 def index_contacts(requests):
+    if requests.method == 'POST':
+        name = requests.POST.get('name')
+        phone = requests.POST.get('phone')
+        message = requests.POST.get('message')
+
+        print(requests)
+        print(f"Имя: {name}\nНомер телефона: {phone}\nСообщение:{message}")
     return render(requests, 'contacts.html')
