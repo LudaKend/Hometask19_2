c39e285040488a583629ddf88644be7a26ef0d1d
LudaKend <Ludakend@gmail.com>
2023-11-16 09:41:33 +0300

    домашнее задание к уроку 20.1 дошла до пункта 4 - добавила и перенсла в БД поле created_at

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Наименование')
+    description = models.TextField(verbose_name='Описание')
+    # здесь картинки товаров, а не то,что пользователь загрузил, поэтому путь такой указала...
+    product_image = models.ImageField(upload_to='static/images/', verbose_name='Изображение')
+    category = models.ForeignKey('Category', on_delete=models.CASCADE, verbose_name='Категория')
+    price = models.FloatField(verbose_name='Цена')
+    data_create = models.DateField(verbose_name='дата создания')
+    data_change = models.DateField(verbose_name='дата изменения')
+
+    def __str__(self):
+        '''строковое отображение обьекта'''
+        return self.name, self.category, self.price
+
+    class Meta:
+        #ordering = ('name')
+        pass
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Наименование')
+    description = models.TextField(verbose_name='Описание')
+    created_at = models.CharField(max_length=10, verbose_name='Новое поле')
+    def __str__(self):
+        return self.name, self.description
+
+    class Meta:
+        verbose_name = 'категория'  #для наименования одного объекта
+        verbose_name_plural = 'категории' #для наименования набора объектов
+        #ordering = ('name')
--- config/urls.py
+++ config/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 # urlpatterns = [
 #     path('admin/', admin.site.urls),
@@ -33,4 +35,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('catalog.urls')),
     path('contacts', include('catalog.urls')),
- ]
+ ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
