944be15df3cd797bb54c62bf69355835cdf94b0a
LudaKend <Ludakend@gmail.com>
2023-12-04 11:30:31 +0300

    домашнее задание к уроку 21.1 файлы views.py, models.py, tests.py, admin.py, __init__.py и файлы из папки migrations не попали в раздел changes, и не были включены в предыдущий commit, поэтому допереношу. также не были перенесены картинки из папки media, последние миграции по модели Product. Кошмар!!! Допереношу.

---
+++ blog/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+NULLABLE = {'null': True, 'blank': True}
+
+class Blog_post(models.Model):
+    head = models.CharField(max_length=100, verbose_name='Заголовок')
+    slug = models.CharField(max_length=100, verbose_name='Слаг')
+    content = models.TextField(verbose_name='Содержимое', **NULLABLE)
+    preview = models.ImageField(upload_to='images/', **NULLABLE, verbose_name='Превью')
+    data_create = models.DateField(verbose_name='дата создания', auto_now_add=True)
+    is_published = models.BooleanField(verbose_name='Опубликован', default=False)
+    views_count = models.IntegerField(verbose_name='Количество просмотров', default=0)
+
+    def __str__(self):
+        '''строковое отображение обьекта'''
+        return f'{self.head}, {self.slug}, {self.is_published}, {self.views_count}'
+
+    class Meta:
+        #ordering = ('name')
+        verbose_name = 'Пост'  # для наименования одного объекта
+        verbose_name_plural = 'Посты в блоге'  # для наименования набора объектов
---
+++ blog/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
+from blog.models import Blog_post
+from django.urls import reverse_lazy
+
+# Create your views here.
+
+class BlogCreateView(CreateView):
+    model = Blog_post
+    fields = ('head', 'slug', 'content', 'preview',)
+
+    success_url = reverse_lazy('blog:list')
+
+class BlogListView(ListView):
+    model = Blog_post
+
+class BlogDetailView(DetailView):
+    model = Blog_post
+
+class BlogUpdateView(UpdateView):
+    model = Blog_post
+    fields = ('head', 'slug', 'content', 'preview', 'is_published',)
+
+    success_url = reverse_lazy('blog:list')
+
+class BlogDeleteView(DetailView):
+    model = Blog_post
+
+    success_url = reverse_lazy('blog:list')
+
