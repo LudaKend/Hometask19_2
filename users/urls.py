d36fc3a30d724e761a1bff506936d3f3959350d6
LudaKend <Ludakend@gmail.com>
2024-01-08 14:54:31 +0300

    домашнее задание к уроку 22.2 задание1 шаблон register.html для регистрации, собственная форма UserRegisterForm

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,12 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 from users.apps import UsersConfig
+from users.views import RegisterView
 
 app_name = UsersConfig.name
 
 urlpatterns = [
     path('', LoginView.as_view(template_name='users/login.html'), name='route_login_users'),
     path('logout/', LogoutView.as_view(), name='route_logout_users'),
+    path('register/', RegisterView.as_view(), name='route_register_users'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import CreateView
+from users.models import User
+from django.contrib.auth.forms import UserCreationForm
+from users.forms import UserRegisterForm
+from django.urls import reverse_lazy
 
-# Create your views here.
+class RegisterView(CreateView):
+    model = User
+    form_class = UserRegisterForm  #UserCreationForm
+    template_name = 'users/register.html'
+
+    success_url = reverse_lazy('users:route_login_users')
+
